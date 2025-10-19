81160e349d93eced958069920e4ec42535772a22
Jagadeesh9122 <jagadeeshbaddila@gmail.com>
2025-10-19 11:38:27 +0530

    frontend url updated in backend

--- store-rating-backend/src/app.js
+++ store-rating-backend/src/app.js
@@ -12,7 +12,7 @@ app.use(morgan('dev'));
 app.use(cors({
   origin: [
     "http://localhost:3000",                        
-    "https://local-store-rating-application.vercel.app" 
+    "https://local-store-rating-application-58vs.vercel.app/" 
   ],
   credentials: true
 }));
